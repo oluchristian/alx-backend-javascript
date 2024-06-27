a666e4bf1ce95ccf73b79102ea3f5243ab57835d
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-06-27 13:13:18 +0100

    No need to create empty objects before adding in properties

--- 0x00-ES6_basic/1-block-scoped copy.js
+++ 0x00-ES6_basic/1-block-scoped copy.js
@@ -4,9 +4,10 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  return {
+  const budget = {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
     [`capita-${getCurrentYear()}`]: capita,
   };
+  return budget;
 }
