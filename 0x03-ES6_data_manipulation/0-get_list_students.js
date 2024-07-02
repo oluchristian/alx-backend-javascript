1cf2bb681c0e4b68dda7c1c915634b4e526cfe5e
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-07-02 04:47:54 +0100

    a function that returns an array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,6 @@
+const getListStudents = () => [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 3, firstName: 'Serena', location: 'San Francisco' },
+];
+export default getListStudents;
