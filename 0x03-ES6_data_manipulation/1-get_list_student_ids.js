3f2fd612f4558b2f50a9fe7096864f8e5bccee78
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-07-02 05:05:01 +0100

    a function Create a function getListStudentIds

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+const getListStudentIds = (students) => {
+  if (Array.isArray(students)) {
+    return students.map((student) => student.id);
+  }
+  return [];
+};
+export default getListStudentIds;
