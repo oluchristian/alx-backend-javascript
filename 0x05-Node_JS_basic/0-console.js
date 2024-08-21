9d7c1b8649374325eca0c889a38a563321a6bbe3
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-08-21 04:11:58 +0100

    Executing basic javascript with Node JS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,9 @@
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+// Arrow function
 function displayMessage(args){
   console.log(args);
 }
